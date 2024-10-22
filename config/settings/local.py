5929cab48adc33330af16874e58672a29326494c
josejuniorpg <yunipgx@protonmail.com>
2024-10-22 15:00:38 -0600

    remove authtool, and add django exctentions.

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,3 +1,22 @@
+# Local imports
 from .base import *
 
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 DEBUG = True
+
+# Applications from base + local
+INSTALLED_APPS += [
+    # Third-party apps
+    'django_extensions',
+]
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
